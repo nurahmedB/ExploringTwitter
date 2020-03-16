850fd0970b912b68f3bf97e9dcc782f178845ca7
Nuruddin Nur Ahmed <59926211+nurahmedB@users.noreply.github.com>
2020-03-16 15:34:21 -0400

    Update DownloadingTweets.R

--- DownloadingTweets.R
+++ DownloadingTweets.R
@@ -19,9 +19,9 @@ twitter_token <- create_token(
   access_token = "14525356-abcdefgh",# copy this info from twitter
   access_secret = "abcdefghijk") #copy this info from twitter
 
-#The below code can take a while depending on "n"; upto 18000 it is pretty fast 
+#The below code can take a while depending on "n"; up to 18000 tweets it is pretty fast 
 df_tweets <- search_tweets(q = "#bitcoin", #Use the hashtag you want to explore
-                           n = 1000000, retryonratelimit=TRUE)
+                           n = 1000000, retryonratelimit=TRUE) #retryonratelimit is the magic variable. It should be set to TRUE to download a large amount of tweets.
 #convert to character
 df_tweets = data.frame(lapply(df_tweets, as.character), stringsAsFactors=FALSE)
 
